8390a848bbd31e539c201d450868de915f991663
Hidayat <hidayat@DESKTOP-4LADPD7>
2023-06-16 23:39:51 +0800

    update file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CompAsia</title>
-    <!-- Bootstrap Icons CSS -->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css"> --}}
 </head>
 
 <body id="app">
